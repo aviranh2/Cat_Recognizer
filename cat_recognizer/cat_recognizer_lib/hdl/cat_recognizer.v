c47e84daf44b3476d8d48c26df7b2b8b52539085
Aviran Huga <Aviranhuga@gmail.com>
2018-11-28 21:33:59 +0200

    finish connect main module

--- cat_recognizer/cat_recognizer_lib/hdl/cat_recognizer.v
+++ cat_recognizer/cat_recognizer_lib/hdl/cat_recognizer.v
@@ -10,63 +10,86 @@
 
 `resetall
 `timescale 1ns/10ps
-module cat_recognizer (PADDR,PENABLE,PSEL,PWDATA,PWRITE,clk,rst,PRDATA,CatRecOut,w_in);
+module CatRecognizer (PADDR,PENABLE,PSEL,PWDATA,PWRITE,clk,rst,PRDATA,CatRecOut,w_in);
   parameter Amba_Word = 24;
   parameter Amba_Addr_Depth = 12;
   parameter Weightrecision = 5;
+  
   input wire PENABLE,PSEL,PWRITE,clk,rst;
   input [Amba_Addr_Depth-1:0] PADDR;
   input [Amba_Word-1:0] PWDATA;
   input [Weight_Percision-1:0] w_in;
   output[Amba_Word-1:0] PRDATA;
   output CatRecOut;
-  wire en_write;
-  wire en_read;
-  wire temp;
-  wire [DATA_WIDTH-1:0] pixels_out;//goes into the calculator
-  wire [Weight_Percision-1:0] weights_out;
+  
+  wire en_write, en_read, temp;
+  wire [DATA_WIDTH-1:0]		 		pixels_bus, control_reg, b;
+  wire [Weight_Percision-1:0] 		weights_bus;
   
   reg [Amba_Word-1:0] b;
+  reg [Amba_Addr_Depth:0] read_address;
+  reg [Amba_Addr_Depth-1:0] mem_address;
+  
+  fsm_apb apb_interface(
+	.pclock 	( clk ),
+	.psel		( PSEL ),
+	.penable	( PENABLE ),
+	.enable 	( en_write )
+  );
   
   RegisterFile #(
-  .DATA_WIDTH(Amba_Word), 
-  .Addr_Depth(Amba_Addr_Depth))
+	.DATA_WIDTH(Amba_Word), 
+	.Addr_Depth(Amba_Addr_Depth))
   pixel_mem(
-  .clock(clk),
-  .en_write(en_write),
-  .en_read(en_read),
-  .address(PADDR),
-  .data_in(PWDATA),
-  .data_out(pixels_out)
+	.clock		(clk),
+	.en_write	(en_write),
+	.en_read	(en_read),
+	.address	(mem_address),
+	.data_in	(PWDATA),
+	.data_out	(pixels_bus),
+	.control_reg (control_reg)
   );
   
   weights_memory #(
-    DATA_WIDTH(Weightrecision),
-	Addr_Depth(Amba_Addr_Depth))
+    .DATA_WIDTH(Weightrecision),
+	.Addr_Depth(Amba_Addr_Depth))
   weight_mem(
-  clock(clk),
-  reset(rst),
-  address(PADDR),
-  en_read(en_read),
-  data_out(weights_out)
+	.clock		(clk),
+	.reset		(rst),
+	.address	(mem_address),
+	.en_read	(en_read),
+	.data_out	(weights_bus)
   );
   
   NeuronCalculator #(
-  .DATA_WIDTH(Amba_Word),
-  .Addr_Depth(Amba_Addr_Depth),
-  .Weight_Percision(Weightrecision))
+	.DATA_WIDTH(Amba_Word),
+	.Addr_Depth(Amba_Addr_Depth),
+	.Weight_Percision(Weightrecision))
   calculator(
-	.clock(clk),
-	.reset(rst),
-	.enable(en_read),
-	.get_result(temp),
-	.x(pixels_out),
-	.b(b),
-	.w(weights_out),
-	.out(CatRecOut)
-  );
-  
-  assign en_write = PSEL&((clk & PWRITE)|(PENABLE & PWRITE));
-  assign en_read = PSEL&(clk|PENABLE);
-  assign temp = !PENABLE;
+	.clock		(clk),
+	.reset		(rst),
+	.enable		(en_read),
+	.get_result	(address[Amba_Addr_Depth),
+	.x			(pixels_bus),
+	.b			(),  // need to connect b
+	.w			(weights_bus),
+	.out		(CatRecOut)
+	);
+
+	always @(posedge clock)
+	begin
+	if(control_reg[0]) // start calc is on
+		if(address[Amba_Addr_Depth])//finish calc
+			en_read 		 <= 1'b0;
+			read_address	 <= {Amba_Addr_Depth{1'b0}}; //reset read address
+		else // didnt finish calc yet
+			en_read 		<= 1'b1;
+			read_address 	<= address + 1;
+		mem_address 		<= read_address[Amba_Addr_Depth-1] // read mode
+	else // in write mode
+		read_address <= {Amba_Addr_Depth{1'b0}};
+		en_read 	 <= 1'b0;  
+		mem_address  <= PADDR; //write mode
+	end
+	
 endmodule
